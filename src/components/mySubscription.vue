053a9dbce02d93d8aa205178fa7227f408a49813
sunchi110 <1301432543@qq.com>
2019-11-19 20:32:47 +0800

    收藏创作用户商城

---
+++ src/components/mySubscription.vue
@@ -0,0 +1,269 @@
+<template>
+  <div>
+    <div>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#fff"
+        text-color="#333"
+        font-size="20px"
+        active-text-color="red">
+        <el-submenu index="1" style="float: right;margin-right: 5%">
+          <template slot="title">admin</template>
+          <el-menu-item index="1-1">
+            <router-link :to="{name:'myCollection'}" style="font-size: 16px"><a>我的收藏</a></router-link>
+          </el-menu-item>
+          <el-menu-item index="1-2">
+            <router-link :to="{name:'mySubscription'}" style="font-size: 16px"><a>我的订阅</a></router-link>
+          </el-menu-item>
+          <el-menu-item index="1-3">
+            <router-link :to="{name:'myCreation'}" style="font-size: 16px"><a>我的创作中心</a></router-link>
+          </el-menu-item>
+          <el-menu-item index="1-4">
+            <router-link :to="{name:'userUpdate'}" style="font-size: 16px"><a>个人中心</a></router-link>
+          </el-menu-item>
+          <el-menu-item index="1-5">注销登陆</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="2" style="float: right">
+          <router-link :to="{name:'order'}" style="font-size: 16px"><a>订单管理</a></router-link>
+        </el-menu-item>
+        <el-menu-item index="3" style="float: right">消息中心</el-menu-item>
+        <el-menu-item style="float: left;margin-left: 5%"><div id="logo">FFF影评网</div></el-menu-item>
+        <el-menu-item index="4" style="float: left;margin-left: 3%">首页</el-menu-item>
+        <el-menu-item index="5" style="float: left">电影</el-menu-item>
+        <el-menu-item index="6" style="float: left">
+          <router-link :to="{name:'creation'}" style="font-size: 16px"><a>创作中心</a></router-link>
+        </el-menu-item>
+        <el-menu-item index="7" style="float: left">
+          <router-link :to="{name:'shop'}" style="font-size: 16px"><a>商城</a></router-link>
+        </el-menu-item>
+
+        <el-menu-item  style="float: right;width: 20%">
+          <el-input placeholder="请输入内容"  v-model="input3" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </el-menu-item>
+      </el-menu>
+    </div>
+
+    <div class="pic">
+      <img src="http://q02obletz.bkt.clouddn.com/tyhj.jpg" style="width: 100%;height: 100px"/>
+    </div>
+
+    <div>
+      <el-row>
+        <el-col :span="8" v-for="(video, index) in video" :key="video" :offset="0">
+          <el-card :body-style="{ padding: '0px' }">
+            <router-link :to="{name:'videoDetail',params:{videoId:video.videoId}}">
+              <img :src="video.videoPic" class="image">
+            </router-link>
+            <div style="padding: 14px;">
+              <div>{{video.videoName}}</div>
+              <div>评分 :  {{video.videoGrade}}</div>
+              <div>
+                <el-button type="primary" icon="el-icon-delete" @click="deleteCollection(video.videoId)">取消收藏</el-button>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+
+</template>
+<script>
+  import axios from 'axios'
+  export default {
+    data() {
+
+      return {
+        currentDate: new Date(),
+        video:[],
+        // videos:{videoId:''}
+      };
+    },
+    mounted(){
+      this.videoQuery()
+    },
+    methods: {
+      videoQuery:function () {
+        axios.get('/api/filmreview-personalcenter/collection/findSubscription').then(res=>{
+          alert(res.data)
+          console.log(res.data)
+          this.video=res.data;
+        })
+      },
+      deleteCollection:function (videoId) {
+        this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          var url = "/api/filmreview-personalcenter/collection/deleteCollection"
+          axios.post(url, {videoId: videoId}).then(res => {
+            if (res.data != null) {
+              this.videoQuery()
+            }
+          })
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      }
+    }
+  };
+</script>
+
+<style>
+  .image {
+    width: 100%;
+    display: block;
+    height: 250px;
+  }
+  .el-row {margin-left: 4%}
+  .el-col-8 {
+    width: 19.33333%;
+  }
+  .el-col-offset-2 {
+    margin-left: 3%;margin-top: 20px;
+  }
+  .el-col-offset-2 {
+    margin-left: 3%;margin-top: 20px;
+  }
+  .el-col-offset-0 {
+    margin-left: 3%;margin-top: 20px;
+  }
+
+
+
+
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
+  }
+
+
+
+
+
+  .pic{width: 90%;height: 100px;margin-left: 5%;margin-right: 5%;background-color: blueviolet}
+
+
+
+
+  .el-container.is-vertical {
+    -webkit-box-orient: vertical;
+    -ms-flex-direction: column;
+    flex-direction: column;
+  }
+  .el-container, .el-container.is-vertical, .el-drawer, .el-link, .el-steps--vertical {
+    -webkit-box-direction: normal;
+  }
+  .el-container {
+    margin-right: 5%;
+  }
+  .el-main {
+
+    padding: 0px;
+    width: 107%;
+  }
+  .el-header {
+    background-color: white;
+    color: #333;
+    line-height: 60px;
+    margin-left: 0%;
+  }
+
+
+  #app{
+    margin-top: 0px;
+  }
+  .el-menu-item{
+    font-size: 17px;
+  }
+  .el-submenu__title{
+    font-size: 17px;
+  }
+  #logo{
+    font-size: 30px;
+  }
+  a{
+    text-decoration: none;
+  }
+
+  .footer {
+    background-color: #262426;
+    padding: 56px 0;
+    margin: 0 auto;
+    min-width: 1200px;
+    margin-top: 82px;
+    color: #ccc;
+  }
+
+  .footer a{
+    color: #EF4238;
+    font-size: 14px;
+  }
+
+  .credentials a{
+    color: #ccc;
+  }
+
+  p {
+    text-color: white;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
+
+
+
+
+  .element.style {
+    width: 70%;
+    transform-origin: left center;
+    z-index: 2017;
+    position: absolute;
+    top: 255px;
+    left: 266px;
+  }
+  .el-button {
+    float: left;
+    margin-left: 10%;
+    margin-top: 60px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import AdminIndex from '@/components/AdminIndex'
-
+import myCollection from '@/components/myCollection'
+import order from '@/components/order'
+import creation from '@/components/creation'
+import userUpdate from '@/components/userUpdate'
+import mySubscription from '@/components/mySubscription'
+import myCreation from '@/components/myCreation'
+import shop from '@/components/shop'
 Vue.use(Router)
 
 export default new Router({
@@ -16,6 +22,41 @@ export default new Router({
       path: '/',
       name: 'AdminIndex',
       component: AdminIndex
+    },
+    {
+      path: '/userUpdate',
+      name: 'userUpdate',
+      component: userUpdate
+    },
+    {
+      path: '/mySubscription',
+      name: 'mySubscription',
+      component: mySubscription
+    },
+    {
+      path: '/myCollection',
+      name: 'myCollection',
+      component: myCollection
+    },
+    {
+      path: '/order',
+      name: 'order',
+      component: order
+    },
+    {
+      path: '/creation',
+      name: 'creation',
+      component: creation
+    },
+    {
+      path: '/myCreation',
+      name: 'myCreation',
+      component: myCreation
+    },
+    {
+      path: '/shop',
+      name: 'shop',
+      component: shop
     }
   ]
 })

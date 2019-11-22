84bc5c63266fa01aaf30495b79c7d594ccba4208
KYD258 <1121991039@qq.com>
2019-11-22 14:57:12 +0800

    main.js修改

--- src/main.js
+++ src/main.js
@@ -5,11 +5,19 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import Axios from 'axios'
+import Axios from 'axios';
+import VideoPlayer from 'vue-video-player';
+import ('video.js/dist/video-js.css');
+import ('vue-video-player/src/custom-theme.css')
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 Vue.prototype.$axios = Axios
+Vue.use(VideoPlayer);
+
+// require('video.js/dist/video-js.css')
+// require('vue-video-player/src/custom-theme.css')
+// require('videojs-contrib-hls/dist/videojs-contrib-hls');
 
 /* eslint-disable no-new */
 new Vue({
